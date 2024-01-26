b36a7602a647cb2747ee503e78d9623a37cc5381
Alex Messina <me@alexmessina.dev>
2024-01-25 21:48:30 -0600

    Project and styling update

---
+++ assets/Alex's Tech Blog_files/dashboard.js
@@ -0,0 +1,90 @@
+const createPostClicked = async (event) =>{
+  event.preventDefault();
+  document.location.replace('/dashboard/create-post');
+}
+
+const newPostHandler = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+    if(title && content){
+      const response = await fetch('/api/bpr/create-post', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } 
+      else {
+        alert(response.statusText);
+      }
+    }  
+  };
+
+const updatePostHandler = async (event) => {
+  event.preventDefault();
+  
+  const title = document.querySelector('#update-post-title').value.trim();
+  const content = document.querySelector('#update-post-content').value.trim();
+  const id = document.querySelector("form").getAttribute('data_id');
+
+  if(title && content){
+    const response = await fetch(`/api/bpr/update-post/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ title, content, id }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    console.log(response)
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } 
+    else {
+      alert(response.statusText);
+    }
+  }  
+}
+
+const deletePostHandler = async (event) => {
+  event.preventDefault();
+  const id = document.querySelector("form").getAttribute('data_id');
+
+  console.log(`/api/bpr/${id}`)
+
+  const response = await fetch(`/api/bpr/${id}`, {
+    method: 'DELETE',
+  });
+
+  console.log(response)
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } 
+  else {
+    alert(response.statusText);
+  }  
+}
+
+if(document.querySelector('#new-post-form') != null){
+  document
+    .querySelector('#new-post-form')
+    .addEventListener('click', createPostClicked); 
+}
+
+if(document.querySelector('#create-post') != null){
+  document
+    .querySelector('#create-post')
+    .addEventListener('click', newPostHandler); 
+}
+
+if(document.querySelector('#update-post') != null){
+  document
+  .querySelector('#update-post')
+  .addEventListener('click', updatePostHandler); 
+}
+
+if(document.querySelector('#delete-post') != null){
+  document
+  .querySelector('#delete-post')
+  .addEventListener('click', deletePostHandler); 
+}
